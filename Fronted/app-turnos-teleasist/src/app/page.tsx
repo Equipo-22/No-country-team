a65f3d1c1a295d89b48b5d447fdc2b789fd88dee
Lauta Di Salvo <lautarodisalvo2023@gmail.com>
2025-10-07 16:00:06 -0300

    feat: register page + axios + mutations

---
+++ Fronted/app-turnos-teleasist/src/app/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { RegisterForm } from "@/_components/Register";
+
+
+export default function Home() {
+
+  return (
+    <div className="flex justify-center g-max-width w-full mx-auto g-height-page">
+      <RegisterForm />
+
+    </div>
+  );
+}

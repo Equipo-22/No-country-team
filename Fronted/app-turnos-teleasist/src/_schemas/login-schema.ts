eabd153a6520786804abf75b17cdfdb3aadcfb9b
Marina Victoria Blanco <marivic_05@hotmail.com>
2025-10-10 11:23:31 -0300

    Validaciones zod en login

--- Fronted/app-turnos-teleasist/src/_schemas/login-schema.ts
+++ Fronted/app-turnos-teleasist/src/_schemas/login-schema.ts
@@ -1,11 +1,14 @@
-import { z } from "zod"
-
+import { z } from "zod";
 
 export const formSchema = z.object({
-    email: z.string().email( {
-        message: "Email no válido",
+  email: z.string().email({
+    message: "Tu correo electrónico no es válido",
+  }),
+  password: z
+    .string()
+    .min(6, { message: "La contraseña debe tener al menos 6 caracteres" })
+    .max(8, { message: "La contraseña no puede superar los 8 caracteres" })
+    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{6,8}$/, {
+      message: "La contraseña debe incluir mayúsculas, minúsculas y números",
     }),
-    password: z.string().min(2, {
-        message: "Username must be at least 2 characters.",
-    })
-})
+});

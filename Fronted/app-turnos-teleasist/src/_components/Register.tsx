26419b75cc73aec207f2dea06f1e7c21302437c8
Marina Victoria Blanco <marivic_05@hotmail.com>
2025-10-16 11:32:43 -0300

    UI/flujo registro exitoso - login - perfil usuario

--- Fronted/app-turnos-teleasist/src/_components/Register.tsx
+++ Fronted/app-turnos-teleasist/src/_components/Register.tsx
@@ -9,7 +9,6 @@ import { Button } from "@/components/ui/button"
 import {
   Form,
   FormControl,
-  /*  FormControl, */
   FormField,
   FormItem,
   FormLabel,
@@ -59,7 +58,7 @@ export default function RegisterForm() {
               control={form.control}
               name="username"
               render={({ field }) => (
-                <FormItem className="">
+                <FormItem>
                   <FormLabel htmlFor="username">Nombre y Apellido</FormLabel>
                   <FormControl>
                     <Input type="text" className="pl-5 bg-[#F2F4F7] placeholder:text-sm" placeholder="Ingresa tu nombre y apellido" id="username"  {...field} />
@@ -72,7 +71,7 @@ export default function RegisterForm() {
               control={form.control}
               name="email"
               render={({ field }) => (
-                <FormItem className="">
+                <FormItem>
                   <FormLabel htmlFor="email">Correo electronico</FormLabel>
                   <FormControl>
                     <Input className="pl-5 bg-[#F2F4F7] placeholder:text-sm" type="email" placeholder="nombre@gmail.com" id="email"   {...field} />
---
+++ Fronted/app-turnos-teleasist/src/app/register/success/page.tsx
@@ -0,0 +1,13 @@
+import SuccessfulRegister from "@/_components/SuccessfulRegister"
+
+const page = () => {
+
+
+    return (
+        <div className="flex justify-center flex-col g-max-width w-full mx-auto g-height-page">
+            <SuccessfulRegister />           
+        </div>
+    )
+}
+
+export default page

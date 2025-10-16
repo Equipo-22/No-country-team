c43e0a842fce7d0216a3a4b708ebd1e01ccdeb02
Marina Victoria Blanco <marivic_05@hotmail.com>
2025-10-16 12:33:02 -0300

    Ajusto estilos en página de bienvenida paciente

--- Fronted/app-turnos-teleasist/src/_components/WelcomePatient.tsx
+++ Fronted/app-turnos-teleasist/src/_components/WelcomePatient.tsx
@@ -1,37 +1,50 @@
 "use client"
 import { Button } from '@/components/ui/button'
+import ContainerMax300 from '@/components/ui/Container-max300'
+import Logo from '@/components/ui/Logo'
+import TitleSection from '@/components/ui/TitleSection'
+import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import React from 'react'
+
 
 const WelcomePatient = () => {
     const router = useRouter()
     const data = [
-        { id: 1, info: "Agendar y gestionar tus citas presenciales o virtuales con tu médico." },
-        { id: 2, info: "Recibir recordatorios automáticos para no olvidar tus turnos." },
-        { id: 3, info: "Acceder a tu historial clínico en cualquier momento." },
-        { id: 4, info: "Conectarte por videollamada en tus consultas online." },
-        { id: 5, info: "Chatear de forma segura con tu profesional de salud" },
+        { id: 1, icon: "/calendar.svg", info: "Agendar y gestionar tus citas presenciales o virtuales con tu médico." },
+        { id: 2, icon: "/notifications.svg", info: "Recibir recordatorios automáticos para no olvidar tus turnos." },
+        { id: 3, icon: "/document.svg", info: "Acceder a tu historial clínico en cualquier momento." },
+        { id: 4, icon: "/videocall.svg", info: "Conectarte por videollamada en tus consultas online." },
+        { id: 5, icon: "/chat.svg", info: "Chatear de forma segura con tu profesional de salud" },
     ]
 
     return (
-        <div className='max-w-[400px] '>
-            <h2 className='text-[#000] text-[2rem] p-[1rem]'>!Te damos la bienvenida¡</h2>
-            <p className='p-[1rem]'>Somos un espacio digital para cuidar tu salud de forma simple, segura y conectada.</p>
-            <b className='text-[#9d9b9b] p-[1rem]'>Con Medihub podes: </b>
-            {
-                data.map(e => (
-                    <section className='p-[1rem]' key={e.id}>
-                        <div className='flex justify-around  gap-4  w-full' >
-                            <p className='bg-[#d9d9d9] rounded-full min-h-[3rem] min-w-[3rem]'></p>
-                            <b>{e.info}</b>
-                        </div>
-                    </section>
-                ))
-            }
-            <p className='p-[1rem]'>Nos enfocamos en brindarte una experiencia fluida, sin complicaciones y con toda la información médica al alcance de tu mano.</p>
-            <p className='p-[1rem] font-bold'>Comenzá explorando tu panel para gestionar tu próxima cita</p>
-            <Button onClick={() => router.push("/dashboard-patient")} className="bg-[#6C757D] p-[2rem] cursor-pointer w-full">Comenzar</Button>
-        </div>
+        <>
+            <Logo />
+            <ContainerMax300 >
+                <TitleSection text="¡Te damos la bienvenida!" />
+                <p className='py-3'>Somos un espacio digital para cuidar tu salud de forma simple, segura y conectada.</p>
+                <b className='text-secondary py-[1rem]'>Con Medihub podes: </b>
+                {
+                    data.map(e => (
+                        <section className='py-2' key={e.id}>
+                            <div className='flex justify-around  gap-3  w-full' >
+                                <Image
+                                    src={e.icon}
+                                    alt=""
+                                    width={63}
+                                    height={63}
+                                />
+                                <p className='text-sm'>{e.info}</p>
+                            </div>
+                        </section>
+                    ))
+                }
+                <p className='py-5'>Nos enfocamos en brindarte una experiencia fluida, sin complicaciones y con toda la información médica al alcance de tu mano.</p>
+                <p className='pb-7 font-bold'>Comenzá explorando tu panel para gestionar tu próxima cita</p>
+                <Button onClick={() => router.push("/dashboard-patient")} className="">Comenzar</Button>
+            </ContainerMax300>
+        </>
     )
 }
 
--- Fronted/app-turnos-teleasist/src/app/welcome-patient/page.tsx
+++ Fronted/app-turnos-teleasist/src/app/welcome-patient/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const page = () => {
     return (
-        <div className='flex justify-center'>
+        <div className='flex justify-center flex-col g-max-width w-full mx-auto g-height-page'>
             <WelcomePatient />
         </div>
     )

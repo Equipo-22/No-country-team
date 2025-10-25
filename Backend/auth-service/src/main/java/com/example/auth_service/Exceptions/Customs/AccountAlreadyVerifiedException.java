8297dd19844c314038f1b47a45f56feb4e92957c
Marina Victoria Blanco <marivic_05@hotmail.com>
2025-10-25 11:57:07 -0300

    Corrijo error de sintaxis

--- Backend/auth-service/src/main/java/com/example/auth_service/Exceptions/Customs/AccountAlreadyVerifiedException.java
+++ Backend/auth-service/src/main/java/com/example/auth_service/Exceptions/Customs/AccountAlreadyVerifiedException.java
@@ -2,6 +2,6 @@ package com.example.auth_service.Exceptions.Customs;
 
 public class AccountAlreadyVerifiedException extends RuntimeException{
     public AccountAlreadyVerifiedException(String email) {
-        super("La cuenta: '"+email"' ya se encuentra verificada. Por favor, inicie sesión");
+        super("La cuenta: '" + email + "' ya se encuentra verificada. Por favor, inicie sesión");
     }
 }

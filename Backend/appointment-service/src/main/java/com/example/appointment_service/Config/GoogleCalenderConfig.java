6e71b288344dea909a61be6ece48976f98e79d9c
Marina Victoria Blanco <marivic_05@hotmail.com>
2025-10-28 09:13:59 -0300

    Credenciales google no expuestas

--- Backend/appointment-service/src/main/java/com/example/appointment_service/Config/GoogleCalenderConfig.java
+++ Backend/appointment-service/src/main/java/com/example/appointment_service/Config/GoogleCalenderConfig.java
@@ -7,6 +7,7 @@ import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.CalendarScopes;
 import com.google.auth.http.HttpCredentialsAdapter;
 import com.google.auth.oauth2.GoogleCredentials;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,21 +20,29 @@ import java.util.Objects;
 public class GoogleCalenderConfig {
 
     private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
-    private static final String WORKSPACE_USER_EMAIL = "medihubb@midominiodemo.shop";
+
+    @Value("${google.credentials.path}")
+    private String credentialsPath;
+
+    @Value("${google.workspace.user}")
+    private String workspaceUser;
+
+    @Value("${google.application.name}")
+    private String applicationName;
 
     @Bean
     public Calendar googleCalenderService() throws IOException, GeneralSecurityException {
-        GoogleCredentials credentials=GoogleCredentials.fromStream(
-                Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("google/credentials.json"))
+        GoogleCredentials credentials = GoogleCredentials.fromStream(
+                Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream(credentialsPath))
         ).createScoped(Collections.singleton(CalendarScopes.CALENDAR))
-                .createDelegated(WORKSPACE_USER_EMAIL);
+         .createDelegated(workspaceUser);
 
         return new Calendar.Builder(
                 GoogleNetHttpTransport.newTrustedTransport(),
                 JSON_FACTORY,
                 new HttpCredentialsAdapter(credentials)
         )
-                .setApplicationName("Appointment Service")
-                .build();
+        .setApplicationName(applicationName)
+        .build();
     }
 }

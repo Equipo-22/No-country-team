da52df57f4f3b7b58cde881064f16b0641cc1127
Marina Victoria Blanco <marivic_05@hotmail.com>
2025-10-29 15:48:43 -0300

    Ajusto estilos boton Nueva cita

--- Fronted/app-turnos-teleasist/src/_components/appointment-item/appointment-item.tsx
+++ Fronted/app-turnos-teleasist/src/_components/appointment-item/appointment-item.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { CirclePlus, ChevronRight, CircleUser } from "lucide-react";
 import TitleSection from "@/components/ui/TitleSection";
+import { useRouter } from "next/navigation";
 
 type AppointmentItemProps = {
   onOpenUpcoming: () => void;
@@ -11,15 +12,16 @@ const AppointmentItem = ({
   onOpenUpcoming,
   onOpenHistory,
 }: AppointmentItemProps) => {
+
+  const router = useRouter()
+
   return (
     <>
-      <div className="flex justify-between items-center">
+      <div className="flex flex-col lg:flex-row justify-between items-center">
+        <Button onClick={() => router.push("/dashboard-patient/appointment/create")} className="w-full lg:w-[200px] lg:order-2 my-6">
+          <CirclePlus /> Nueva cita
+        </Button>
         <TitleSection text="Próximas citas" />
-        <a href="/dashboard-patient/appointment/create">
-          <Button>
-            <CirclePlus /> Nueva cita
-          </Button>
-        </a>
       </div>
 
       <div className="flex flex-col gap-2 px-4 py-2 rounded-sm shadow-sm bg-white my-8 lg:px-8 lg:py-4 lg:gap-0 lg:items-center lg:flex-row lg:justify-between">

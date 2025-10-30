e4f956c7fc8d737df98c698a05d805a5101bdf94
Marina Victoria Blanco <marivic_05@hotmail.com>
2025-10-30 19:51:54 -0300

    Agrego anotación para que cambie estado de cita

--- Fronted/app-turnos-teleasist/src/_components/AppointmentCalendar.tsx
+++ Fronted/app-turnos-teleasist/src/_components/AppointmentCalendar.tsx
@@ -1,4 +1,5 @@
 import { AppointmentMutationsService } from "@/_service/use-mutation-services/appointment-mutation-services";
+import { AppointmentType } from "@/_types/appointment-type";
 import { Calendar } from "@/components/ui/calendar";
 import { es } from "date-fns/locale";
 import { useEffect, useState } from "react";
@@ -9,21 +10,27 @@ type AppointmentCalendarProps = {
 
 export default function AppointmentCalendar({ idPatient }: AppointmentCalendarProps) {
   const [date, setDate] = useState<Date | undefined>(new Date());
-  const [appointments, setAppointments] = useState<Date[]>([])
+  const [futureAppointments, setFutureAppointments] = useState<Date[]>([])
 
   const { mutationGetAppointmentsByIdPatient } = AppointmentMutationsService()
 
 
-  useEffect(() => {
-    if (!idPatient) return;
+useEffect(() => {
+  if (!idPatient) return;
 
-    mutationGetAppointmentsByIdPatient.mutate(idPatient, {
-      onSuccess: (data) => {
-        const startDates = data.map((a: any) => new Date(a.startTime));
-        setAppointments(startDates);
-      },
-    });
-  }, [idPatient]);
+  mutationGetAppointmentsByIdPatient.mutate(idPatient, {
+    onSuccess: (data: AppointmentType[]) => {
+      const now = new Date();
+
+      const startDates = data.map((a: any) => new Date(a.startTime));
+
+      const futureAppointments = startDates.filter(date => date >= now);
+
+      setFutureAppointments(futureAppointments);
+
+    },
+  });
+}, [idPatient]);
 
 
   return (
@@ -35,7 +42,7 @@ export default function AppointmentCalendar({ idPatient }: AppointmentCalendarPr
       className="w-full rounded-md border shadow-sm [&_.rdp-day:focus]:bg-red"
       captionLayout="dropdown"
       modifiers={{
-        booked: appointments,
+        booked: futureAppointments,
       }}
       modifiersClassNames={{
         booked: "border-2 border-secondary text-secondary font-bold rounded-full hover:bg-secondary hover:text-white transition-colors",

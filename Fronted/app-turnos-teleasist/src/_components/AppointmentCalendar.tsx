373320e235f75f07f5c12b5b30aef0a85f7c5f8d
Marina Victoria Blanco <marivic_05@hotmail.com>
2025-10-30 14:05:09 -0300

    Próximas citas marcadas en calendario de dashboard

---
+++ Fronted/app-turnos-teleasist/src/_components/AppointmentCalendar.tsx
@@ -0,0 +1,45 @@
+import { AppointmentMutationsService } from "@/_service/use-mutation-services/appointment-mutation-services";
+import { Calendar } from "@/components/ui/calendar";
+import { es } from "date-fns/locale";
+import { useEffect, useState } from "react";
+
+type AppointmentCalendarProps = {
+  idPatient: string
+}
+
+export default function AppointmentCalendar({ idPatient }: AppointmentCalendarProps) {
+  const [date, setDate] = useState<Date | undefined>(new Date());
+  const [appointments, setAppointments] = useState<Date[]>([])
+
+  const { mutationGetAppointmentsByIdPatient } = AppointmentMutationsService()
+
+
+  useEffect(() => {
+    if (!idPatient) return;
+
+    mutationGetAppointmentsByIdPatient.mutate(idPatient, {
+      onSuccess: (data) => {
+        const startDates = data.map((a: any) => new Date(a.startTime));
+        setAppointments(startDates);
+      },
+    });
+  }, [idPatient]);
+
+
+  return (
+    <Calendar
+      locale={es}
+      mode="single"
+      selected={date}
+      onSelect={setDate}
+      className="w-full rounded-md border shadow-sm [&_.rdp-day:focus]:bg-red"
+      captionLayout="dropdown"
+      modifiers={{
+        booked: appointments,
+      }}
+      modifiersClassNames={{
+        booked: "border-2 border-secondary text-secondary font-bold rounded-full hover:bg-secondary hover:text-white transition-colors",
+      }}
+    />
+  );
+}
--- Fronted/app-turnos-teleasist/src/_components/patient/Patient.tsx
+++ Fronted/app-turnos-teleasist/src/_components/patient/Patient.tsx
@@ -1,19 +1,17 @@
 "use client"
 
 import { Button } from '@/components/ui/button'
-import { Calendar } from '@/components/ui/calendar'
 import TitleSection from '@/components/ui/TitleSection'
 import { useUserStore } from '@/store/userStore'
-import { es } from 'date-fns/locale'
 import { useRouter } from 'next/navigation'
-import React, { useState } from 'react'
+import React from 'react'
+import AppointmentCalendar from '../AppointmentCalendar'
 
 export const Patient = () => {
 
   const router = useRouter()
-  const [date, setDate] = useState<Date | undefined>(new Date())
 
-  const { username } = useUserStore()
+  const { username, idPatient } = useUserStore()
 
   const firstName = username
     ? username.split(" ")[0].charAt(0).toUpperCase() + username.split(" ")[0].slice(1).toLowerCase()
@@ -29,14 +27,7 @@ export const Patient = () => {
           <Button className='w-full md:w-60 lg:w-80 lg:h-12 lg:text-lg rounded text-secondary bg-white hover:bg-secondary hover:text-white p-2.5 my-4 lg:my-8 cursor-pointer'
             onClick={() => router.push("/dashboard-patient/appointment")} >Ver proximas citas</Button>
         </div>
-        <Calendar
-          locale={es}
-          mode="single"
-          selected={date}
-          onSelect={setDate}
-          className="w-full rounded-md border shadow-sm"
-          captionLayout="dropdown"
-        />
+        <AppointmentCalendar idPatient={idPatient}/>
       </div>
     </div>
   )

6afa0646bce1be4c853ee554f5c55f87b9729c67
Daniela Londono <daniela0393h@gmail.com>
2025-10-30 20:55:32 -0500

    se traen proximas citas y generar nueva cita

--- Fronted/app-turnos-teleasist/src/_components/appointment-item/appointment-item.tsx
+++ Fronted/app-turnos-teleasist/src/_components/appointment-item/appointment-item.tsx
@@ -4,73 +4,92 @@ import TitleSection from "@/components/ui/TitleSection";
 import { useRouter } from "next/navigation";
 
 type AppointmentItemProps = {
-  onOpenUpcoming: () => void;
-  onOpenHistory: () => void;
+  onOpenUpcoming: (id: string) => void;
+  onOpenHistory: (id: string) => void;
+  doctors: {
+    content: any[];
+  };
+  appointments: any[];
 };
 
 const AppointmentItem = ({
   onOpenUpcoming,
   onOpenHistory,
+  doctors,
+  appointments = [],
 }: AppointmentItemProps) => {
+  const router = useRouter();
 
-  const router = useRouter()
+  const upcomingAppointments = appointments.filter(
+    (appointment: any) => new Date() < new Date(appointment.startTime)
+  );
 
+  const historyAppointments = appointments.filter(
+    (appointment: any) => new Date() > new Date(appointment.startTime)
+  );
   return (
     <>
       <div className="flex flex-col lg:flex-row justify-between items-center">
-        <Button onClick={() => router.push("/dashboard-patient/appointment/create")} className="w-full lg:w-[200px] lg:order-2 my-6">
+        <Button
+          onClick={() => router.push("/dashboard-patient/appointment/create")}
+          className="w-full lg:w-[200px] lg:order-2 my-6"
+        >
           <CirclePlus /> Nueva cita
         </Button>
         <TitleSection text="Próximas citas" />
       </div>
 
-      <div className="flex flex-col gap-2 px-4 py-2 rounded-sm shadow-sm bg-white my-8 lg:px-8 lg:py-4 lg:gap-0 lg:items-center lg:flex-row lg:justify-between">
-        <div className="flex items-center gap-2 lg:items-center">
-          <div className="w-10 h-10 rounded-full bg-gray-200 flex items-center justify-center">
-            <CircleUser className="text-gray-500" />
+      {upcomingAppointments.map((appointment: any) => (
+        <div key={appointment.id} className="flex flex-col gap-2 px-4 py-2 rounded-sm shadow-sm bg-white my-8 lg:px-8 lg:py-4 lg:gap-0 lg:items-center lg:flex-row lg:justify-between">
+          <div className="flex items-center gap-2 lg:items-center">
+            <div className="w-10 h-10 rounded-full bg-gray-200 flex items-center justify-center">
+              <CircleUser className="text-gray-500" />
+            </div>
+            <p>
+              <strong>{doctors?.content?.find((doctor: any) => doctor.id === appointment.professionalId)?.name}</strong>
+            </p>
           </div>
-          <p>
-            <strong>Juan Cruz Gómez </strong>
-          </p>
+          <p>{doctors?.content?.find((doctor: any) => doctor.id === appointment.professionalId)?.specialty}</p>
+          <p>{new Date(appointment.startTime).toLocaleDateString()}</p>
+          <p>{new Date(appointment.endTime).toLocaleTimeString()}</p>
+          <Button variant="outline">{appointment.status}</Button>
+          <Button variant="outline">{appointment.type}</Button>
+          <button
+            type="button"
+            className="cursor-pointer"
+            onClick={() => onOpenUpcoming(appointment.id)}
+          >
+            <ChevronRight />
+          </button>
         </div>
-        <p>Traumatología</p>
-        <p>02/11/25</p>
-        <p>09:30hs</p>
-        <Button variant="outline">Cita confirmada</Button>
-        <Button variant="outline">Presencial</Button>
-        <button
-          type="button"
-          className="cursor-pointer"
-          onClick={onOpenUpcoming}
-        >
-          <ChevronRight />
-        </button>
-      </div>
+      ))}
 
       <TitleSection text="Historial de citas" />
 
-      <div className="flex flex-col gap-2 px-4 py-2 rounded-sm shadow-sm bg-white my-8 lg:px-8 lg:py-4 lg:gap-0 lg:items-center lg:flex-row lg:justify-between">
-        <div className="flex items-center gap-2 lg:items-center">
-          <div className="w-10 h-10 rounded-full bg-gray-200 flex items-center justify-center">
-            <CircleUser className="text-gray-500" />
+      {historyAppointments.map((appointment) => (
+        <div key={appointment.id} className="flex flex-col gap-2 px-4 py-2 rounded-sm shadow-sm bg-white my-8 lg:px-8 lg:py-4 lg:gap-0 lg:items-center lg:flex-row lg:justify-between">
+          <div className="flex items-center gap-2 lg:items-center">
+            <div className="w-10 h-10 rounded-full bg-gray-200 flex items-center justify-center">
+              <CircleUser className="text-gray-500" />
+            </div>
+            <p>
+              <strong>{doctors?.content?.find((doctor: any) => doctor.id === appointment.professionalId)?.name}</strong>
+            </p>
           </div>
-          <p>
-            <strong>Juan Cruz Gómez </strong>
-          </p>
+          <p>{doctors?.content?.find((doctor: any) => doctor.id === appointment.professionalId)?.specialty}</p>
+          <p>{new Date(appointment.startTime).toLocaleDateString()}</p>
+          <p>{new Date(appointment.endTime).toLocaleTimeString()}</p>
+          <Button variant="outline">{appointment.status}</Button>
+          <Button variant="outline">{appointment.type}</Button>
+          <button
+            type="button"
+            className="cursor-pointer"
+            onClick={() => onOpenHistory(appointment.id)}
+          >
+            <ChevronRight />
+          </button>
         </div>
-        <p>Traumatología</p>
-        <p>02/11/25</p>
-        <p>09:30hs</p>
-        <Button variant="outline">Cita confirmada</Button>
-        <Button variant="outline">Presencial</Button>
-        <button
-          type="button"
-          className="cursor-pointer"
-          onClick={onOpenHistory}
-        >
-          <ChevronRight />
-        </button>
-      </div>
+      ))}
     </>
   );
 };
--- Fronted/app-turnos-teleasist/src/app/dashboard-patient/appointment/create/page.tsx
+++ Fronted/app-turnos-teleasist/src/app/dashboard-patient/appointment/create/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
-import { CircleUser, CalendarDays } from "lucide-react";
+import { CalendarDays } from "lucide-react";
 import Modal from "@/components/ui/modal";
 import AppointmentModeSelector from "@/_components/appointment-mode-selector/appointment-mode-selector";
 import AppointmentDateSelection from "@/_components/appointment-date-selection/appointment-date-selection";
@@ -14,6 +14,25 @@ type AppointmentModalProps = {
   onClose: () => void;
   isVirtual: boolean;
   onNext: () => void;
+  doctors: {
+    content: any[];
+  };
+  setSelectedCategory: (category: string) => void;
+  setSelectedPlace: (place: string) => void;
+  setSelectedDoctor: (doctor: string) => void;
+  selectedCategory: string;
+  selectedPlace: string;
+  selectedDoctor: string;
+  selectedMotivo: string;
+  setSelectedMotivo: (motivo: string) => void;
+};
+
+const getDoctors = async () => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_URL_BASE}/doctor/doctor?page=0&size=100`
+  );
+  const data = await response.json();
+  return data;
 };
 
 const AppointmentCreate = () => {
@@ -22,6 +41,28 @@ const AppointmentCreate = () => {
   const [step, setStep] = useState<
     "mode" | "date" | "confirmation" | "success"
   >("mode");
+  const [doctors, setDoctors] = useState<any>([]);
+  const [apptType, setApptType] = useState<"VIRTUAL" | "PRESENCIAL" | null>(
+    null
+  );
+  const [selectedDoctor, setSelectedDoctor] = useState<any>(null);
+  const [selectedCategory, setSelectedCategory] = useState<any>(null);
+  const [selectedPlace, setSelectedPlace] = useState<any>(null);
+  const [selectedMotivo, setSelectedMotivo] = useState<any>(null);
+
+  console.log({
+    apptType,
+    selectedDoctor,
+    selectedCategory,
+    selectedPlace,
+    selectedMotivo,
+  });
+
+  useEffect(() => {
+    getDoctors().then((data) => {
+      setDoctors(data);
+    });
+  }, []);
 
   const goToDateSelection = () => {
     setStep("date");
@@ -34,49 +75,117 @@ const AppointmentCreate = () => {
   const goToSuccess = () => setStep("success");
 
   return (
-      <>
-        <div className="flex flex-col w-full">
-          {step === "mode" && (
-            <AppointmentModeSelector
-              onOpenPresencial={() => setOpenPresencialModal(true)}
-              onOpenVirtual={() => setOpenVirtualModal(true)}
-            />
-          )}
-          {step === "date" && (
-            <AppointmentDateSelection onNext={goToConfirmation} />
-          )}
-          {step === "confirmation" && (
-            <AppointmentConfirmation
-              onBack={goToDateSelection}
-              onConfirm={goToSuccess}
-            />
-          )}
-          {step === "success" && <AppointmentConfirmationSuccess />}
-        </div>
+    <>
+      <div className="flex flex-col w-full">
+        {step === "mode" && (
+          <AppointmentModeSelector
+            onOpenPresencial={() => {
+              setOpenPresencialModal(true);
+              setApptType("PRESENCIAL");
+            }}
+            onOpenVirtual={() => {
+              setOpenVirtualModal(true);
+              setApptType("VIRTUAL");
+            }}
+          />
+        )}
+        {step === "date" && (
+          <AppointmentDateSelection onNext={goToConfirmation}
+          selectedDoctor={doctors?.content?.find((doctor: any) => doctor.id === selectedDoctor)}
+          
+          />
+        )}
+        {step === "confirmation" && (
+          <AppointmentConfirmation
+            onBack={goToDateSelection}
+            onConfirm={goToSuccess}
+          />
+        )}
+        {step === "success" && <AppointmentConfirmationSuccess />}
+      </div>
 
-        <AppointmentModal
-          isOpen={openPresencialModal}
-          onClose={() => setOpenPresencialModal(false)}
-          isVirtual={false}
-          onNext={goToDateSelection}
-        />
+      <AppointmentModal
+        isOpen={openPresencialModal}
+        onClose={() => setOpenPresencialModal(false)}
+        isVirtual={false}
+        onNext={goToDateSelection}
+        doctors={doctors}
+        setSelectedCategory={setSelectedCategory}
+        setSelectedPlace={setSelectedPlace}
+        setSelectedDoctor={setSelectedDoctor}
+        selectedCategory={selectedCategory}
+        selectedPlace={selectedPlace}
+        selectedDoctor={selectedDoctor}
+        selectedMotivo={selectedMotivo}
+        setSelectedMotivo={setSelectedMotivo}
+      />
 
-        <AppointmentModal
-          isOpen={openVirtualModal}
-          onClose={() => setOpenVirtualModal(false)}
-          isVirtual={true}
-          onNext={goToDateSelection}
-        />
-      </>
+      <AppointmentModal
+        isOpen={openVirtualModal}
+        onClose={() => setOpenVirtualModal(false)}
+        isVirtual={true}
+        onNext={goToDateSelection}
+        doctors={doctors}
+        setSelectedCategory={setSelectedCategory}
+        setSelectedPlace={setSelectedPlace}
+        setSelectedDoctor={setSelectedDoctor}
+        selectedCategory={selectedCategory}
+        selectedPlace={selectedPlace}
+        selectedDoctor={selectedDoctor}
+        selectedMotivo={selectedMotivo}
+        setSelectedMotivo={setSelectedMotivo}
+      />
+    </>
   );
 };
 
 const AppointmentModal = ({
   isOpen,
-  onClose,
   isVirtual,
+  doctors,
+  setSelectedCategory,
+  setSelectedPlace,
+  setSelectedDoctor,
+  selectedCategory,
+  selectedPlace,
+  selectedDoctor,
   onNext,
+  onClose,
+  selectedMotivo,
+  setSelectedMotivo,
 }: AppointmentModalProps) => {
+  const handleNext = () => {
+    if (
+      selectedCategory === "Seleccionar categoría" ||
+      selectedCategory === "" ||
+      selectedCategory === null
+    ) {
+      alert("Por favor selecciona una categoría.");
+      return;
+    }
+    if (!isVirtual && !selectedPlace) {
+      alert("Por favor selecciona un lugar.");
+      return;
+    }
+    if (
+      selectedDoctor === "Seleccionar médico" ||
+      selectedDoctor === "" ||
+      selectedDoctor === null
+    ) {
+      alert("Por favor selecciona un médico.");
+      return;
+    }
+    if (
+      selectedMotivo === "Seleccionar motivo" ||
+      selectedMotivo === "" ||
+      selectedMotivo === null
+    ) {
+      alert("Por favor selecciona un motivo.");
+      return;
+    }
+    onNext();
+  };
+
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <div className="space-y-4">
@@ -85,14 +194,17 @@ const AppointmentModal = ({
             Categoría<span className="text-red-500">*</span>
           </label>
           <select
+            required
             name="category"
-            onChange={onClose}
+            onChange={(e) => setSelectedCategory(e.target.value)}
             className="w-full border border-gray-300 rounded-lg p-2 text-sm"
           >
             <option value="">Seleccionar categoría</option>
-            <option value="general">Medicina General</option>
-            <option value="odontologia">Odontología</option>
-            <option value="pediatria">Pediatría</option>
+            {doctors?.content?.map((doctor: any) => (
+              <option key={doctor.id} value={doctor.id}>
+                {doctor.specialty}
+              </option>
+            ))}
           </select>
         </div>
 
@@ -102,8 +214,9 @@ const AppointmentModal = ({
               Lugar<span className="text-red-500">*</span>
             </label>
             <select
+              required
               name="place"
-              onChange={onClose}
+              onChange={(e) => setSelectedPlace(e.target.value)}
               className="w-full border border-gray-300 rounded-lg p-2 text-sm"
             >
               <option value="">Seleccionar lugar</option>
@@ -118,24 +231,37 @@ const AppointmentModal = ({
             Médico<span className="text-red-500">*</span>
           </label>
           <select
+            required
             name="doctor"
-            onChange={onClose}
+            onChange={(e) => setSelectedDoctor(e.target.value)}
             className="w-full border border-gray-300 rounded-lg p-2 text-sm"
           >
             <option value="">Seleccionar médico</option>
-            <option value="juan_perez">Dr. Juan Pérez</option>
-            <option value="ana_gomez">Dra. Ana Gómez</option>
+            {doctors?.content?.map((doctor: any) => (
+              <option key={doctor.id} value={doctor.id}>
+                {doctor.name}
+              </option>
+            ))}
           </select>
         </div>
 
+        <div>
+          <label className="block text-sm font-medium text-gray-700 mb-1">
+            Motivo<span className="text-red-500">*</span>
+          </label>
+
+          <textarea
+            required
+            name="motivo"
+            onChange={(e) => setSelectedMotivo(e.target.value)}
+            className="w-full border border-gray-300 rounded-lg p-2 text-sm"
+          />
+        </div>
+
         <div className="flex flex-col gap-3 pt-4">
-          <Button onClick={onNext}>
+          <Button onClick={handleNext}>
             <CalendarDays className="mr-2" />
             Ver días y horarios disponibles
-          </Button>
-          <Button variant="outline">
-            <CircleUser className="mr-2" />
-            Ver perfil del médico
           </Button>
         </div>
       </div>
